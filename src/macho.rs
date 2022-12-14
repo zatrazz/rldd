4912b76f482b9e83898a083281c5e1664402586f
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2022-12-14 12:12:14 -0300

    macho.rs: Add @rpath support
    The LD_RPATH load commands are read for each binary and dependencies.
    It also requires some refactoring to test if the rpath created path
    exit, so the library is not registered if rpath generates an non
    existent path.

--- src/macho.rs
+++ src/macho.rs
@@ -10,6 +10,7 @@ use object::Endianness;
 use crate::deptree::*;
 use crate::pathutils;
 use crate::search_path;
+use crate::search_path::SearchPathVecExt;
 
 static MACOS_BIG_SUR_CACHE_PATH_ARM64: &str =
     "/System/Volumes/Preboot/Cryptexes/OS/System/Library/dyld/dyld_shared_cache_arm64e";
@@ -28,7 +29,7 @@ pub fn create_context() -> DyldCache {
         _ => return DyldCache::new(),
     };
 
-    if let Ok(elc) = open_macho_file(&Path::new(path)) {
+    if let Ok(elc) = open_macho_file(&Path::new(path), &String::new()) {
         if let Some(cache) = elc.cache {
             return cache;
         }
@@ -46,29 +47,30 @@ pub fn resolve_binary(
     arg: &str,
 ) -> Result<DepTree, std::io::Error> {
     let filename = Path::new(arg).canonicalize()?;
-    let executable_path: String = filename
-        .parent()
-        .and_then(|s| s.to_str())
-        .and_then(|s| Some(s.to_string()))
-        .unwrap_or(String::new());
 
-    let elc = open_macho_file(&filename)?;
+    let executable_path = pathutils::get_path(&filename).unwrap();
+
+    let elc = open_macho_file(&filename, &executable_path)?;
 
     let mut deptree = DepTree::new();
-
     let depp = deptree.addroot(DepNode {
         path: Some(executable_path.clone()),
-        name: filename
-            .file_name()
-            .and_then(|s| s.to_str())
-            .unwrap_or("")
-            .to_string(),
+        name: pathutils::get_name(&filename),
         mode: DepMode::Executable,
         found: false,
     });
 
     for dep in &elc.deps {
-        resolve_dependency(cache, &executable_path, &dep, &mut deptree, depp, all);
+        resolve_dependency(
+            cache,
+            &executable_path,
+            &executable_path,
+            &elc.rpath,
+            &dep,
+            &mut deptree,
+            depp,
+            all,
+        );
     }
 
     Ok(deptree)
@@ -76,6 +78,7 @@ pub fn resolve_binary(
 
 #[derive(Debug)]
 struct MachOInfo {
+    rpath: search_path::SearchPathVec,
     deps: DepsVec,
     cache: Option<DyldCache>,
 }
@@ -84,78 +87,177 @@ type DepsVec = Vec<String>;
 fn resolve_dependency(
     cache: &HashSet<String>,
     executable_path: &String,
+    loader_path: &String,
+    rpaths: &search_path::SearchPathVec,
     dependency: &String,
     deptree: &mut DepTree,
     depp: usize,
     all: bool,
 ) {
-    let dependency = dependency.replace("@executable_path", executable_path);
-    // TODO: @loader_path/
-    // TODO: @rpath/
+    let mut dependency = dependency.replace("@executable_path", executable_path);
+    dependency = dependency.replace("@loader_path", loader_path);
 
+    if dependency.contains("@rpath") {
+        for rpath in rpaths {
+            let mut newdependency = dependency.replace("@rpath", rpath.path.as_str());
+            if resolve_dependency_1(
+                cache,
+                executable_path,
+                &mut newdependency,
+                true,
+                deptree,
+                depp,
+                all,
+            ) {
+                return;
+            }
+        }
+        return;
+    }
+
+    resolve_dependency_1(
+        cache,
+        executable_path,
+        &mut dependency,
+        false,
+        deptree,
+        depp,
+        all,
+    );
+}
+
+fn resolve_dependency_1(
+    cache: &HashSet<String>,
+    executable_path: &String,
+    dependency: &mut String,
+    rpath: bool,
+    deptree: &mut DepTree,
+    depp: usize,
+    all: bool,
+) -> bool {
+    let elc = resolve_dependency_2(
+        cache,
+        executable_path,
+        dependency,
+        rpath,
+        deptree,
+        depp,
+        all,
+    );
+    if let Some((elc, name)) = elc {
+        let path = pathutils::get_path(&dependency).unwrap_or(String::new());
+
+        let c = deptree.addnode(
+            DepNode {
+                path: Some(path.clone()),
+                name: name,
+                mode: DepMode::Direct,
+                found: false,
+            },
+            depp,
+        );
+
+        for dep in &elc.deps {
+            resolve_dependency(
+                cache,
+                executable_path,
+                &path,
+                &elc.rpath,
+                &dep,
+                deptree,
+                c,
+                all,
+            );
+        }
+        true
+    } else {
+        false
+    }
+}
+
+fn resolve_dependency_2(
+    cache: &HashSet<String>,
+    executable_path: &String,
+    dependency: &mut String,
+    rpath: bool,
+    deptree: &mut DepTree,
+    depp: usize,
+    all: bool,
+) -> Option<(MachOInfo, String)> {
     // Try to read the library file contents.
     let path = Path::new(&dependency);
     let elc = if path.is_absolute() {
-        open_macho_file(&path).handle_err()
+        open_macho_file(&path, executable_path).handle_err()
     } else {
         None
     };
 
     // The dependency library does not exist on filesystem, check the dydl cache.
-    let dependency = if elc.is_none() {
-        let mode = if !cache.contains(&dependency) {
-            DepMode::NotFound
-        } else {
-            DepMode::LdSoConf
-        };
-        deptree.addnode(
-            DepNode {
-                path: pathutils::get_path(&path),
-                name: pathutils::get_name(&path),
-                mode: mode,
-                found: false,
-            },
-            depp,
-        );
-        return;
+    let path = if elc.is_none() {
+        if !rpath {
+            let mode = if !cache.contains(dependency) {
+                DepMode::NotFound
+            } else {
+                // Check if dependency is already found.
+                if resolve_dependency_check_found(dependency, deptree, depp, all) {
+                    return None;
+                }
+                DepMode::LdSoConf
+            };
+            deptree.addnode(
+                DepNode {
+                    path: pathutils::get_path(&path),
+                    name: pathutils::get_name(&path),
+                    mode: mode,
+                    found: false,
+                },
+                depp,
+            );
+        }
+        return None;
     } else {
         path.canonicalize().unwrap()
     };
 
-    let name = pathutils::get_name(&dependency);
+    let name = pathutils::get_name(&path);
+    *dependency = path.to_string_lossy().to_string();
 
     // Check if dependency is already found.
-    if let Some(entry) = deptree.get(&dependency.to_string_lossy().to_string()) {
+    if resolve_dependency_check_found(dependency, deptree, depp, all) {
+        return None;
+    }
+
+    Some((elc.unwrap(), name))
+}
+
+fn resolve_dependency_check_found(
+    dependency: &String,
+    deptree: &mut DepTree,
+    depp: usize,
+    all: bool,
+) -> bool {
+    if let Some(entry) = deptree.get(&dependency) {
         if all {
             deptree.addnode(
                 DepNode {
                     path: entry.path,
-                    name: name,
+                    name: entry.name,
                     mode: entry.mode.clone(),
                     found: true,
                 },
                 depp,
             );
         }
-        return;
-    }
-
-    let c = deptree.addnode(
-        DepNode {
-            path: pathutils::get_path(&dependency),
-            name: name,
-            mode: DepMode::Direct,
-            found: false,
-        },
-        depp,
-    );
-
-    for dep in &elc.unwrap().deps {
-        resolve_dependency(cache, executable_path, &dep, deptree, c, all);
+        true
+    } else {
+        false
     }
 }
 
-fn open_macho_file<'a, P: AsRef<Path>>(filename: &P) -> Result<MachOInfo, std::io::Error> {
+fn open_macho_file<'a, P: AsRef<Path>>(
+    filename: &P,
+    executable_path: &String,
+) -> Result<MachOInfo, std::io::Error> {
     let file = match fs::File::open(&filename) {
         Ok(file) => file,
         Err(_) => return Err(Error::new(ErrorKind::Other, "Failed to open file")),
@@ -166,28 +268,23 @@ fn open_macho_file<'a, P: AsRef<Path>>(filename: &P) -> Result<MachOInfo, std::i
         Err(_) => return Err(Error::new(ErrorKind::Other, "Failed to map file")),
     };
 
-    let parent = match filename.as_ref().parent().and_then(Path::to_str) {
-        Some(parent) => parent,
-        None => "",
-    };
-
-    match parse_object(&*mmap, parent) {
+    match parse_object(&*mmap, executable_path) {
         Ok(elc) => Ok(elc),
         Err(e) => return Err(Error::new(ErrorKind::Other, e)),
     }
 }
 
-fn parse_object(data: &[u8], _origin: &str) -> Result<MachOInfo, &'static str> {
+fn parse_object(data: &[u8], executable_path: &String) -> Result<MachOInfo, &'static str> {
     let kind = match object::FileKind::parse(data) {
         Ok(file) => file,
         Err(_err) => return Err("Failed to parse file"),
     };
 
     match kind {
-        object::FileKind::MachO32 => parse_macho32(data),
-        object::FileKind::MachO64 => parse_macho64(data),
-        object::FileKind::MachOFat32 => parse_macho_fat32(data),
-        object::FileKind::MachOFat64 => parse_macho_fat64(data),
+        object::FileKind::MachO32 => parse_macho32(data, executable_path),
+        object::FileKind::MachO64 => parse_macho64(data, executable_path),
+        object::FileKind::MachOFat32 => parse_macho_fat32(data, executable_path),
+        object::FileKind::MachOFat64 => parse_macho_fat64(data, executable_path),
         object::FileKind::DyldCache => parse_dyld_cache(data),
         _ => Err("Invalid object"),
     }
@@ -206,30 +303,30 @@ impl<T, E: fmt::Display> HandleErr<T> for Result<T, E> {
     }
 }
 
-fn parse_macho32(data: &[u8]) -> Result<MachOInfo, &'static str> {
+fn parse_macho32(data: &[u8], executable_path: &String) -> Result<MachOInfo, &'static str> {
     if let Some(macho) = MachHeader32::parse(data, 0).handle_err() {
-        return parse_macho(macho, data);
+        return parse_macho(macho, data, executable_path);
     }
     Err("Invalid Mach-O 32 object")
 }
 
-fn parse_macho64(data: &[u8]) -> Result<MachOInfo, &'static str> {
+fn parse_macho64(data: &[u8], executable_path: &String) -> Result<MachOInfo, &'static str> {
     if let Some(macho) = MachHeader64::parse(data, 0).handle_err() {
-        return parse_macho(macho, data);
+        return parse_macho(macho, data, executable_path);
     }
     Err("Invalid Mach-O 64 object")
 }
 
-fn parse_macho_fat32(data: &[u8]) -> Result<MachOInfo, &'static str> {
+fn parse_macho_fat32(data: &[u8], executable_path: &String) -> Result<MachOInfo, &'static str> {
     if let Some(arches) = FatHeader::parse_arch32(data).handle_err() {
-        return parse_macho_fat(data, arches);
+        return parse_macho_fat(data, arches, executable_path);
     }
     Err("Invalid FAT Mach-O 32 object")
 }
 
-fn parse_macho_fat64(data: &[u8]) -> Result<MachOInfo, &'static str> {
+fn parse_macho_fat64(data: &[u8], executable_path: &String) -> Result<MachOInfo, &'static str> {
     if let Some(arches) = FatHeader::parse_arch64(data).handle_err() {
-        return parse_macho_fat(data, arches);
+        return parse_macho_fat(data, arches, executable_path);
     }
     Err("Invalid FAT Mach-O 64 object")
 }
@@ -250,11 +347,12 @@ fn check_current_arch(arch: object::Architecture) -> bool {
 fn parse_macho_fat<FatArch: object::read::macho::FatArch>(
     data: &[u8],
     arches: &[FatArch],
+    executable_path: &String,
 ) -> Result<MachOInfo, &'static str> {
     for arch in arches {
         if check_current_arch(arch.architecture()) {
             if let Some(fatdata) = arch.data(data).handle_err() {
-                return parse_object(fatdata, "");
+                return parse_object(fatdata, executable_path);
             }
         }
     }
@@ -264,20 +362,28 @@ fn parse_macho_fat<FatArch: object::read::macho::FatArch>(
 fn parse_macho<Mach: MachHeader<Endian = Endianness>>(
     header: &Mach,
     data: &[u8],
+    executable_path: &String,
 ) -> Result<MachOInfo, &'static str> {
     let mut deps = DepsVec::new();
+    let mut rpaths = search_path::SearchPathVec::new();
 
     if let Ok(endian) = header.endian() {
         if let Ok(mut commands) = header.load_commands(endian, data, 0) {
             while let Ok(Some(command)) = commands.next() {
-                if let Some(dep) = parse_load_command::<Mach>(endian, command) {
-                    deps.push(dep);
+                match parse_load_command::<Mach>(endian, command) {
+                    Some((LoadCommand::DYLIB, dylib)) => deps.push(dylib),
+                    Some((LoadCommand::RPATH, rpath)) => {
+                        let rpath = rpath.replace("@executable_path", executable_path);
+                        rpaths.add_path(rpath.as_str());
+                    }
+                    _ => {}
                 }
             }
         }
     }
 
     Ok(MachOInfo {
+        rpath: rpaths,
         deps: deps,
         cache: None,
     })
@@ -303,25 +409,43 @@ fn parse_dyld_cache(data: &[u8]) -> Result<MachOInfo, &'static str> {
     }
 
     Ok(MachOInfo {
+        rpath: search_path::SearchPathVec::new(),
         deps: DepsVec::new(),
         cache: Some(cache),
     })
 }
 
+enum LoadCommand {
+    DYLIB,
+    RPATH,
+}
+
+fn parse_string<'data>(data: Option<&'data [u8]>) -> Option<String> {
+    data.and_then(|s| str::from_utf8(s).ok().and_then(|s| Some(s.to_string())))
+}
+
 fn parse_load_command<Mach: MachHeader>(
     endian: Mach::Endian,
     command: LoadCommandData<Mach::Endian>,
-) -> Option<String> {
+    //) -> Option<String> {
+) -> Option<(LoadCommand, String)> {
     if let Ok(variant) = command.variant() {
         match variant {
             LoadCommandVariant::Dylib(x) | LoadCommandVariant::IdDylib(x) => {
-                return command
-                    .string(endian, x.dylib.name)
-                    .ok()
-                    .and_then(|s| str::from_utf8(s).ok().and_then(|s| Some(s.to_string())))
+                if let Some(dylib) = parse_string(command.string(endian, x.dylib.name).ok()) {
+                    return Some((LoadCommand::DYLIB, dylib));
+                };
+                None
             }
-            _ => {}
+            LoadCommandVariant::Rpath(x) => {
+                if let Some(rpath) = parse_string(command.string(endian, x.path).ok()) {
+                    return Some((LoadCommand::RPATH, rpath));
+                };
+                None
+            }
+            _ => None,
         }
+    } else {
+        None
     }
-    None
 }
