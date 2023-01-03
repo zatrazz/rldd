8da20c92776415882ac917fc85cbab6667ae7694
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2023-01-03 09:53:34 -0300

    elf: Simplify system_dirs path creation
    Use vec! macro when possible.

--- src/elf/system_dirs.rs
+++ src/elf/system_dirs.rs
@@ -1,16 +1,15 @@
 #[cfg(any(target_os = "linux", target_os = "illumos", target_os = "solaris"))]
 use object::elf::*;
 
-/* Not all machines are supported by object crate.  */
-#[cfg(target_os = "linux")]
-const EM_ARCV2: u16 = 195;
-
 use crate::search_path;
 
 // Return the default system directory for the architectures and class.  It is hard
 // wired on glibc install for each triplet (the $slibdir).
 #[cfg(target_os = "linux")]
 pub fn get_slibdir(e_machine: u16, ei_class: u8) -> Option<&'static str> {
+    // Not all machines are supported by object crate.
+    const EM_ARCV2: u16 = 195;
+
     match e_machine {
         EM_AARCH64 | EM_ALPHA | EM_PPC64 | EM_LOONGARCH => Some("/lib64"),
         EM_ARCV2 | EM_ARM | EM_CSKY | EM_PARISC | EM_386 | EM_68K | EM_MICROBLAZE
@@ -36,73 +35,69 @@ pub fn get_slibdir(e_machine: u16, ei_class: u8) -> Option<&'static str> {
 
 #[cfg(target_os = "linux")]
 pub fn get_system_dirs(e_machine: u16, ei_class: u8) -> Option<search_path::SearchPathVec> {
-    let mut r = search_path::SearchPathVec::new();
-
     let path = get_slibdir(e_machine, ei_class)?;
-    r.push(search_path::SearchPath {
-        path: path.to_string(),
-        dev: 0,
-        ino: 0,
-    });
-    // The '/usr' part is configurable on glibc install, however there is no direct
-    // way to obtain it on runtime.
-    // TODO: Add an option to override it.
-    r.push(search_path::SearchPath {
-        path: format!("/usr/{}", path.to_string()),
-        dev: 0,
-        ino: 0,
-    });
-
-    Some(r)
+    Some(vec![
+        search_path::SearchPath {
+            path: path.to_string(),
+            dev: 0,
+            ino: 0,
+        },
+        // The '/usr' part is configurable on glibc install, however there is no direct
+        // way to obtain it on runtime.
+        // TODO: Add an option to override it.
+        search_path::SearchPath {
+            path: format!("/usr/{}", path.to_string()),
+            dev: 0,
+            ino: 0,
+        },
+    ])
 }
 
 #[cfg(target_os = "freebsd")]
 pub fn get_system_dirs(_e_machine: u16, _ei_class: u8) -> Option<search_path::SearchPathVec> {
-    let mut r = search_path::SearchPathVec::new();
-    r.push(search_path::SearchPath {
+    Some(vec![search_path::SearchPath {
         path: "/lib".to_string(),
         dev: 0,
         ino: 0,
-    });
-    Some(r)
+    }])
 }
 
 #[cfg(any(target_os = "openbsd", target_os = "netbsd"))]
 pub fn get_system_dirs(_e_machine: u16, _ei_class: u8) -> Option<search_path::SearchPathVec> {
-    let mut r = search_path::SearchPathVec::new();
-    r.push(search_path::SearchPath {
+    Some(vec![search_path::SearchPath {
         path: "/usr/lib".to_string(),
         dev: 0,
         ino: 0,
-    });
-    Some(r)
+    }])
 }
 
 #[cfg(any(target_os = "illumos", target_os = "solaris"))]
 pub fn get_system_dirs(e_machine: u16, _ei_class: u8) -> Option<search_path::SearchPathVec> {
-    let mut r = search_path::SearchPathVec::new();
-    if e_machine == EM_386 {
-        r.push(search_path::SearchPath {
-            path: "/lib".to_string(),
-            dev: 0,
-            ino: 0,
-        });
-        r.push(search_path::SearchPath {
-            path: "/usr/lib".to_string(),
-            dev: 0,
-            ino: 0,
-        });
-    } else if e_machine == EM_X86_64 {
-        r.push(search_path::SearchPath {
-            path: "/lib/64".to_string(),
-            dev: 0,
-            ino: 0,
-        });
-        r.push(search_path::SearchPath {
-            path: "/usr/lib/64".to_string(),
-            dev: 0,
-            ino: 0,
-        });
-    };
-    Some(r)
+    match e_machine {
+        EM_386 => Some(vec![
+            search_path::SearchPath {
+                path: "/lib".to_string(),
+                dev: 0,
+                ino: 0,
+            },
+            search_path::SearchPath {
+                path: "/usr/lib".to_string(),
+                dev: 0,
+                ino: 0,
+            },
+        ]),
+        EM_X86_64 => Some(vec![
+            search_path::SearchPath {
+                path: "/lib64".to_string(),
+                dev: 0,
+                ino: 0,
+            },
+            search_path::SearchPath {
+                path: "/usr/lib/64".to_string(),
+                dev: 0,
+                ino: 0,
+            },
+        ]),
+        _ => None,
+    }
 }
