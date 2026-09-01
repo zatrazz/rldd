1a865863a896e982f2944b70220b3f9e6dc466c3
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2026-09-01 08:48:25 -0300

    elf: Fix the MIPS N32 detection on the ld.so.cache flags check
    The glibc ldconfig flags N32 objects based on EF_MIPS_ABI2 (0x20), not
    EF_MIPS_ABI_ON32 (0x40).

--- src/elf/ld_so_cache.rs
+++ src/elf/ld_so_cache.rs
@@ -131,13 +131,13 @@ fn check_file_entry_flags(
         },
         EM_MIPS => match ei_class {
             ELFCLASS32 => {
-                if e_flags & (EF_MIPS_NAN2008 | EF_MIPS_ABI_ON32)
-                    == EF_MIPS_NAN2008 | EF_MIPS_ABI_ON32
+                if e_flags & (EF_MIPS_NAN2008 | EF_MIPS_ABI2)
+                    == EF_MIPS_NAN2008 | EF_MIPS_ABI2
                 {
                     flags == FLAG_MIPS64_LIBN32_NAN2008 | FLAG_ELF_LIBC6
                 } else if e_flags & EF_MIPS_NAN2008 == EF_MIPS_NAN2008 {
                     flags == FLAG_MIPS_LIB32_NAN2008 | FLAG_ELF_LIBC6
-                } else if e_flags & EF_MIPS_ABI_ON32 == EF_MIPS_ABI_ON32 {
+                } else if e_flags & EF_MIPS_ABI2 == EF_MIPS_ABI2 {
                     flags == FLAG_MIPS64_LIBN32 | FLAG_ELF_LIBC6
                 } else {
                     flags == FLAG_ELF_LIBC6
